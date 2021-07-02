bed47cdf7160a0845940cb9f525446a1fd804346
yaowamuz.c@gmail.com <yaowamuz.c@gmail.com>
2021-07-03 00:02:33 +0700

    [Vue3]update finish 2/7/21

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+//  import "bootsttrap/dist/css/bootstrap.css";
+// import "bootsttrap/dist/js/bootstrap.bundle.js";
+
+// admin :template \
+import "startbootstrap-sb-admin/dist/css/styles.css";
+import "startbootstrap-sb-admin/dist/js/scripts.js";
+
 createApp(App).use(store).use(router).mount("#app");
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+  <Logo />
   </div>
 </template>
+<script>
+import Logo from "@/components/Logo.vue";
+export default {
+  name: "About",
+  components: {
+    Logo,
+  },
+};
+</script>
+<style scoped>
+.about {
+  color: red;
+}
+</style>
